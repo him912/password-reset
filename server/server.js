db741bf070e7d364600dac0cd924a7f395c72f11
himasnhu1 <himanshusaini712@gmail.com>
2026-05-21 17:02:38 +0530

    done

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const connetDB = require("./config/db");
+
+dotenv.config();
+
+connetDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Incoming Request: ${req.method} ${req.url}`);
+  console.log("Request Body:", req.body);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Password-reset MVC API");
+});
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+console.log(`PORT URI: ${process.env.PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
